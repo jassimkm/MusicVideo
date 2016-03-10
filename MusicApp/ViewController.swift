f144799042f7b213ef3cfea95d0375132dfed1b3
Jassim Mukthar <jassimkm@jassims-macbook-air.local>
2016-03-10 15:37:15 +0530

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Jassim Mukthar on 10/03/16.
+//  Copyright © 2016 Jassim Mukthar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
